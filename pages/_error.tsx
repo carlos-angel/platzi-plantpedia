335ebd14dab34274e8eb6c5f322ecab0510a6675
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-11-29 11:07:16 -0600

    refactor: agregar la traducción en la páginas de error

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,4 +1,6 @@
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
 import { Layout } from '@components/Layout'
 import { Typography } from '@ui/Typography'
@@ -11,7 +13,13 @@ type ErrorPageProps = {
   message?: string
 }
 
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+  props: await serverSideTranslations(locale!),
+})
+
 const ErrorPage: NextPage<ErrorPageProps> = ({ statusCode, message }) => {
+  const { t } = useTranslation(['page-errors'])
+
   if (statusCode === 404) {
     return <NotFound />
   }
@@ -22,9 +30,7 @@ const ErrorPage: NextPage<ErrorPageProps> = ({ statusCode, message }) => {
 
   let errorMessage = message
   if (!message) {
-    errorMessage = statusCode
-      ? 'An error occurred on the server'
-      : 'An error occurred on the client'
+    errorMessage = statusCode ? t('serverError') : t('clientError')
   }
 
   return (
@@ -38,7 +44,7 @@ const ErrorPage: NextPage<ErrorPageProps> = ({ statusCode, message }) => {
         </Typography>
         {!statusCode ? null : (
           <Typography variant="body1" className="mb-6">
-            <span className="bg-gray-300 inline-block">
+            <span className="inline-block bg-gray-300">
               ERRORCODE: {statusCode}
             </span>
           </Typography>
@@ -47,9 +53,9 @@ const ErrorPage: NextPage<ErrorPageProps> = ({ statusCode, message }) => {
           color="primary"
           variant="contained"
           href="/"
-          title="Go back home"
+          title={t('goHome')}
         >
-          Go back home
+          {t('goHome')}
         </Button>
       </div>
     </Layout>
